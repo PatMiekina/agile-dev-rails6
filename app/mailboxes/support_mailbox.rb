2b643c7d14aca3bab906df9df96262795d6f14c3
Patrycja Miekina <patmiekina@gmail.com>
2023-04-12 19:53:11 +0100

    fix email format in support request & add tests

--- app/mailboxes/support_mailbox.rb
+++ app/mailboxes/support_mailbox.rb
@@ -1,9 +1,11 @@
 class SupportMailbox < ApplicationMailbox
   def process
+    clean_email = mail.from.to_s.match(/([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})/)
+
     recent_order = Order.where(email: mail.from).order('created_at desc').first
 
     request = SupportRequest.create!(
-      email:    mail.from.to_s,
+      email:    clean_email,
       subject:  mail.subject,
       body:     mail.body.to_s,
       order:    recent_order
